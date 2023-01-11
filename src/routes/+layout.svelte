817778700b6fd650d3b5e1405311fe321dc3ffc0
Victor Kane <victorkane@gmail.com>
2023-01-11 10:56:34 -0300

    style(branding): basic branding and scaffold nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,14 @@
 </script>
 
 <div class="bg-gray-50 text-slate-500 grid h-screen place-content-center">
-	<slot />
+	<nav class="m-4">
+		<a href="/">
+			<img class="block h-12 w-auto md:hidden" src="/icon170.png" alt="Your Company" />
+			<img class="hidden h-12 w-auto md:block" src="/logo.png" alt="Your Company" /></a
+		>
+		<a href="/process">Process</a>
+	</nav>
+	<main>
+		<slot />
+	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,1 @@
-<h1 class="text-8xl">Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<h1 class="text-4xl lg:text-8xl">Welcome to AWebFactory Project Flow & Tracker</h1>
